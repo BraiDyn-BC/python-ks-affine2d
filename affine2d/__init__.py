031269c13a2837cf9a56e92cc0d90f38e9985c75
Keisuke Sehara <keisuke.sehara@gmail.com>
2024-06-03 15:37:44 +0900

    modify license terms

--- affine2d/__init__.py
+++ affine2d/__init__.py
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 """functions related to Affine transform."""
 from typing import Optional, Tuple, Union
 
